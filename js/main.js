9da84d83b5492964c4c47c611752b43ba1f86ef8
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-04-25 14:46:52 -0500

    Carpeta assets con todas las imagenes a utilizar, y carpeta js/main.js con la funcionalidad

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    
+    $.post('php/lecturas.php', function(res){
+        res = JSON.parse(res);
+        //console.log(res);
+        var temperatura = res[0]['valor'];
+        var humedad = res[1]['valor'];
+        var intensidad = res[2]['valor'];
+        console.log(temperatura);
+        $("#temp").text(temperatura);
+        $("#hum").text(humedad);
+        $("#luz").text(intensidad);
+    })
+
+    $.post('php/lecturas.php', function(res){
+        res = JSON.parse(res);
+        //console.log(res);
+        var temperatura = res[0]['valor'];
+        var humedad = res[1]['valor'];
+        var intensidad = res[2]['valor'];
+        
+        if(temperatura >= 10){
+            $('#cloudy').hide();
+            $('#sunny').hide();
+        }
+        else if(temperatura >= 10 && temperatura <= 25){
+            $('#sunny').hide();
+            $('#snowflake').hide();
+        }
+        else if(temperatura > 25){
+            $('#cloudy').hide();
+            $('#snowflake').hide();
+        }
+        
+        if(humedad < 33){
+            $('#hum1').hide();
+            $('#hum2').hide();
+        }
+        else if(humedad >= 33 && humedad <= 66){
+            $('#hum1').hide();
+            $('#hum3').hide();
+        }
+        else if(humedad > 66){
+            $('#hum1').hide();
+            $('#hum2').hide();
+        }
+
+        if(intensidad <= 50){
+            $('#pw_on').hide();
+        }
+        else if(intensidad > 50){
+            $('#pw_off').hide();
+        }
+
+    })
+
+});
