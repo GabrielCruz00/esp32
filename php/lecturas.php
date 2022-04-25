0d20bcdcf921cdea24860d3232406a38359ba99b
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-04-25 14:54:03 -0500

    Archivo que regresa los datos de la Base de Datos

---
+++ php/lecturas.php
@@ -0,0 +1,24 @@
+<?php
+
+// $conexion = new mysqli("localhost", "id18809090_tlacua00", "", "id18809090_dsm" );
+    $conexion = new mysqli("localhost", "root", "", "dsm" );
+    //1$respuesta = ["res" => true];
+
+    if(!$conexion){
+        $respuesta = ["res" => false];
+    } else {
+        // $respuesta = ["res" => true];
+        $sql = "SELECT * FROM lecturas";
+
+        $resultado = $conexion -> query($sql);
+
+        foreach($resultado as $valor) {
+            $respuesta[] = ['id' => $valor['id'],
+                            'nombre' => $valor['sensor'],
+                            'valor' => $valor['medicion']];
+        }
+        $resultado -> free();
+    }
+
+    echo json_encode($respuesta);
+?>
