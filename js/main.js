f604158286da3993167a55cf5e52fe56b49dbd2a
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-05-09 16:41:21 -0500

    Modificaciones a la actualización de la pagina index.html. En lugar de recargar la pagina, vuelve a correr el script con la función del botón actualizar

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     
     $.post('php/lecturas.php', function(res){
         res = JSON.parse(res);
-        //console.log(res);
         var temperatura = res[0]['valor'];
         var humedad = res[1]['valor'];
         var intensidad = res[2]['valor'];
@@ -10,52 +9,102 @@ $(document).ready(function(){
         $("#temp").text(temperatura);
         $("#hum").text(humedad);
         $("#luz").text(intensidad);
-    });
 
-    $.post('php/lecturas.php', function(res){
-        res = JSON.parse(res);
-        console.log(res);
-        var temperatura = res[0]['valor'];
-        var humedad = res[1]['valor'];
-        var intensidad = res[2]['valor'];
-        
         if(temperatura < 10){
             $('#cloudy').hide();
             $('#sunny').hide();
+            $('#snowflake').show();
         }
         else if(temperatura >= 10 && temperatura <= 25){
             $('#sunny').hide();
             $('#snowflake').hide();
+            $('#cloudy').show();
         }
         else if(temperatura > 25){
             $('#cloudy').hide();
             $('#snowflake').hide();
+            $('#sunny').show();
         }
         
         if(humedad < 33){
             $('#hum1').hide();
             $('#hum2').hide();
+            $('#hum3').show();
         }
         else if(humedad >= 33 && humedad <= 66){
             $('#hum1').hide();
             $('#hum3').hide();
+            $('#hum2').show();
         }
         else if(humedad > 66){
             $('#hum1').hide();
             $('#hum2').hide();
+            $('#hum3').show();
         }
 
         if(intensidad <= 50){
             $('#pw_on').hide();
+            $('#pw_off').show();
         }
         else if(intensidad > 50){
             $('#pw_off').hide();
+            $('#pw_on').show();
         }
     });
 
     $('#btnActualizar').click(function(){
-        //console.log('Actualizar');
-        location.reload();
+        //location.reload();
+        $.post('php/lecturas.php', function(res){
+            res = JSON.parse(res);
+            var temperatura = res[0]['valor'];
+            var humedad = res[1]['valor'];
+            var intensidad = res[2]['valor'];
+            
+            $("#temp").text(temperatura);
+            $("#hum").text(humedad);
+            $("#luz").text(intensidad);
+    
+            if(temperatura < 10){
+                $('#cloudy').hide();
+                $('#sunny').hide();
+                $('#snowflake').show();
+            }
+            else if(temperatura >= 10 && temperatura <= 25){
+                $('#sunny').hide();
+                $('#snowflake').hide();
+                $('#cloudy').show();
+            }
+            else if(temperatura > 25){
+                $('#cloudy').hide();
+                $('#snowflake').hide();
+                $('#sunny').show();
+            }
+            
+            if(humedad < 33){
+                $('#hum1').hide();
+                $('#hum2').hide();
+                $('#hum3').show();
+            }
+            else if(humedad >= 33 && humedad <= 66){
+                $('#hum1').hide();
+                $('#hum3').hide();
+                $('#hum2').show();
+            }
+            else if(humedad > 66){
+                $('#hum1').hide();
+                $('#hum2').hide();
+                $('#hum3').show();
+            }
+    
+            if(intensidad <= 50){
+                $('#pw_on').hide();
+                $('#pw_off').show();
+            }
+            else if(intensidad > 50){
+                $('#pw_off').hide();
+                $('#pw_on').show();
+            }
+        });
     });
 
 });
